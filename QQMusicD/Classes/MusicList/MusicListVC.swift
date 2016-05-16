68d23e5cd2a25a96bcff54603f3ddcd83c7a6c63
YuePr <yuepr@yueprdemacbook-pro.local>
2016-05-16 21:34:38 +0800

    第一次项目初始化，采用MVC设计模式，相关icon资源是从腾讯相关app获取

---
+++ QQMusicD/Classes/MusicList/MusicListVC.swift
@@ -0,0 +1,38 @@
+//
+//  MusicListVC.swift
+//  QQMusicD
+//
+//  Created by YuePr on 16/5/16.
+//  Copyright © 2016年 YuePr. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("---888******")
+        view.backgroundColor = UIColor.redColor()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
